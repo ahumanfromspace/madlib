f01692f9b42d77a40e47a5c3a23bfdc74b26bb11
ahumanfromspace <myra0810.10@gmail.com>
2024-11-12 12:30:00 -0500

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -1,3 +1,12 @@
-
-story = "The other day my friend and I spotted a/an" + adjective1 + " looking noun1 on the sidewalk. My friend decided to" + verb1 + " it while I " + verb2 + "-ed. It was " + adjective2 + ". I am quite " + adjective3 + ". Anyways then we stole a/an " + noun2 +  " and it was very" + adjective4 + ".”
 print("Hello! Welcome to my Madlib game!")
+adjective1 = input("Enter an Adjective:")
+adjective2 = input("Enter an Adjective:")
+adjective3 = input("Enter an Adjective:")
+adjective4 = input("Enter an Adjective:")
+
+noun1 = input("Enter a Noun:")
+noun2 = input("Enter a Noun:")
+
+verb1 = input("Enter a Verb:")
+verb2 = input("Enter a Verb:")
+story = "The other day my friend and I spotted a/an" + adjective1 + " looking " + noun1 + " on the sidewalk. My friend decided to" + verb1 + " it while I " + verb2 + "-ed. It was " + adjective2 + ". I am quite " + adjective3 + ". Anyways then we stole a/an " + noun2 +  " and it was very" + adjective4 + ".”
