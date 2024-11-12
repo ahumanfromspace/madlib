23d5848a3816c5ba674348d5a7ed33eb13520a95
ahumanfromspace <myra0810.10@gmail.com>
2024-11-12 11:35:37 -0500

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -1,3 +1,3 @@
+
 story = “The other day my friend and I spotted a/an {adjective1} looking {noun1} on the sidewalk. My friend decided to {verb1} it while I {verb2}-ed. It was {adjective2}. I am quite {adjective3}. Anyways then we stole a/an {noun2} and it was very {adjective4}.”
 print("Hello! Welcome to my Madlib game!")
-input("")
